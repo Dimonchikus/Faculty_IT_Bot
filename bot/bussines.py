115cad4cd0c7e478468fe9676c840de87d11eeb8
dimon <dimonchikakus@gmail.com>
2018-03-08 17:44:16 +0200

    4

--- bot/bussines.py
+++ bot/bussines.py
@@ -13,18 +13,6 @@ def start(message):
     start_flag = True
     print('start')
 
-
-def stop(message):
-    hide_markup = telebot.types.ReplyKeyboardRemove(True)
-    Bot.send_message(message.from_user.id, '...', reply_markup=hide_markup)
-    print('Бот зупинено')
-
-
-# user_markup = telebot.types.ReplyKeyboardMarkup(True)
-#     user_markup.row('Відмітка про перебування у звільненні(добове)', 'Сповістити про зауваження чи загрозу')
-#     Bot.send_message(message.from_user.id, 'Введіть дані \n(Прізвище Ініціали Група)', reply_markup=user_markup)
-
-
 def add_cadet(message):
     global start_flag
     with open('bd.txt', 'a') as fio:
