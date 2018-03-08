a7d482f101676ba0ebab18e5b28ea589dd0b854a
dimon <dimonchikakus@gmail.com>
2018-03-08 17:38:18 +0200

    3

--- bot/main.py
+++ bot/main.py
@@ -4,6 +4,7 @@ from bot import constants
 
 Bot = telebot.TeleBot(constants.token_of_bot)
 
+
 @bussines.Bot.message_handler(commands=['start'])
 def handle_start(message):
     bussines.start(message)
@@ -11,19 +12,14 @@ def handle_start(message):
 
 @bussines.Bot.message_handler(content_types=['text'])
 def handle_text(message):
-    if message.text == "stop":
-        bussines.stop(message)
-
-    elif message.text == "'Відмітка про перебування поза межами інституту":
+    if message.text == "'Відмітка про перебування поза межами інституту":
         Bot.send_message(message.from_user.id, 'Введіть ПБІ')
     elif message.text == "Сповістити про зауваження чи загрозу":
         Bot.send_message(message.from_user.id, 'Опишіть стан дій')
+
 
 try:
     b = bussines.Bot.polling(none_stop=True, interval=0)
     print(b)
 except ConnectionError:
     print("Aborted Connection")
-
-
-
