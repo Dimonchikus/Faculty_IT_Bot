53abcb270c8497f773b1d6925f26fa4119909c59
Sergey Gorbenko <sergey13gorbenko@gmail.com>
2018-03-09 18:18:03 +0200

    1

--- bot/constants.py
+++ bot/constants.py
@@ -1,1 +1,3 @@
 token_of_bot = '550062128:AAEv-y9N0uQas3ciiLM6v3mRYD8-uxCFUsI'
+google_map_url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='
+google_map_url_2 = '&key=AIzaSyBv0ySt0JK5CUxHbI01cCTFLVNh-7BSdno'
