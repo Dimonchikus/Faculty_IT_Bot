e4032f72817f4b3f343eda25ebdbcde4d85209de
Sergey Gorbenko <sergey13gorbenko@gmail.com>
2018-03-09 18:30:57 +0200

    1

--- bot/main.py
+++ bot/main.py
@@ -1,4 +1,6 @@
 import telebot
+import requests
+import json
 from telebot.types import Location
 
 from bot import bussines
@@ -13,8 +15,6 @@ def handle_start(message):
     bussines.start(message)
 
 
-
-
 @bussines.Bot.message_handler(content_types=['text'])
 def handle_text(message):
     if bussines.start_flag:
@@ -22,7 +22,8 @@ def handle_text(message):
     elif message.text == "Відмітка про перебування поза межами інституту":
         bussines.Bot.send_message(message.from_user.id, 'Ви відмічені')
     elif message.text == "Сповістити про зауваження чи загрозу":
-        bussines.Bot.send_message(message.from_user.id, 'Опишіть коротко, що трапилося. За можливості пришліть своє місцезнаходження')
+        bussines.Bot.send_message(message.from_user.id,
+                                  'Опишіть коротко, що трапилося. За можливості пришліть своє місцезнаходження')
         global if_alert
         if_alert = True;
 
@@ -30,11 +31,14 @@ def handle_text(message):
 @bussines.Bot.message_handler(content_types=["location"])
 def get_location(message):
     global if_alert
-    if(if_alert == True):
+    if (if_alert == True):
+        r = requests.get(constants.google_map_url + str(message.location.latitude) + ',' + str(message.location.longitude) + constants.google_map_url_2)
+        j = r.json()
+        print(j)
         print("Done")
-        bussines.Bot.send_location(message.from_user.id,message.location.latitude,message.location.longitude)
-        if_alert = False
+        bussines.Bot.send_location(message.from_user.id, message.location.latitude, message.location.longitude)
 
+        if_alert = False
 
 
 try:
@@ -42,6 +46,3 @@ try:
     print(b)
 except ConnectionError:
     print("Aborted Connection")
-
-
-
